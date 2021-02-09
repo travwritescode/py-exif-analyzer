c2ae5a4510118827aa0d4ea9095518e5f701a1d3
travwritescode <travis.anderson30@gmail.com>
2021-02-08 20:10:15 -0700

    Got pickling and unpickling working for a single directory

--- main.py
+++ main.py
@@ -2,43 +2,64 @@ import exifread
 import pprint
 import pickle
 from pathlib import Path
+from os import listdir
 from exifread.tags import FIELD_TYPES
 
-
-filename = 'Street Photos 0226 - 2766'
+PICKLE_STORE_PATH = './pickle_store/'
 
 
 def scrapeData(file_list):
+    metadata_dict = {}
     for file in file_list:
         f = file.open('rb')
         metadata_list = exifread.process_file(f)
-    # f = open(file_path, 'rb')
+        metadata_dict[file.name] = metadata_list
 
-    dict = {}
-    for f in metadata_list:
-        dict[file]
-
-    return metadata_list
+    return metadata_dict
 
 
-def trim_metadata(image_metadata):
-    for tag in image_metadata.keys():
-        pprint.pprint(image_metadata[tag])
-        #print(image_metadata[tag].printable)
+def pickle_exif_data(store):
+    for key in store.keys():
+        filename = key
+        outfile = open('./pickle_store/' + filename, 'wb')
+        pickle.dump(store[key], outfile)
+        outfile.close()
 
 
-def pickle_exif_data(metadata):
-    outfile = open(filename, 'wb')
-    pickle.dump(metadata, outfile)
-    outfile.close()
-
-
-def unpickle_exif_data(file):
-    infile = open(filename, 'rb')
-    loaded_metadata = pickle.load(infile)
-    infile.close()
+def unpickle_exif_data():
+    pickles = listdir('./pickle_store/')
+    print(pickles)
+    for p in pickles:
+        infile = open(PICKLE_STORE_PATH + p, 'rb')
+        loaded_metadata = pickle.load(infile)
+        infile.close()
     return loaded_metadata
 
+
+def import_files():
+    p = Path('E:/2020/0106').resolve()
+    path_list = list(p.glob('**/*.PEF'))
+    return path_list
+
+
+if __name__ == '__main__':
+    # files = import_files()
+    # dict_store = scrapeData(files)
+    # exif = scrapeData('Street Photos 0226 - 2766.jpg')
+    # pickle_exif_data(dict_store)
+    unpickled = unpickle_exif_data(filename)
+    print_metadata(unpickled)
+
+
+
+
+
+'''
+ExposureTime
+FNumber
+ISOSpeedRatings
+FocalLength
+'''
 
 def print_metadata(image_metadata):
     #print(type(image_metadata))
@@ -52,28 +73,7 @@ def print_metadata(image_metadata):
             #pprint.pprint(len(tag))
              #pprint.pprint(FIELD_TYPES[image_metadata[tag].field_type])
 
-
-def import_files():
-    p = Path('E:/2020/0106').resolve()
-    path_list = list(p.glob('**/*.PEF'))
-    return path_list
-
-
-if __name__ == '__main__':
-    files = import_files()
-    metadata_list = scrapeData(files)
-    # exif = scrapeData('Street Photos 0226 - 2766.jpg')
-    # pickle_exif_data(exif)
-    # unpickled = unpickle_exif_data(filename)
-    # print_metadata(unpickled)
-
-
-
-
-
-'''
-ExposureTime
-FNumber
-ISOSpeedRatings
-FocalLength
-'''
+def trim_metadata(image_metadata):
+    for tag in image_metadata.keys():
+        pprint.pprint(image_metadata[tag])
+        #print(image_metadata[tag].printable)
